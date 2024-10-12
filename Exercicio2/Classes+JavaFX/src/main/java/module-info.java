098dd351d3bc389f0bc8c9ef8c175312c93c1083
Gustavo Mendes <162648811+GustavoNMendes01@users.noreply.github.com>
2024-10-12 13:41:35 -0300

    delete paste

--- Exercicio2/Classes+JavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.atividadelp1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.atividadelp1 to javafx.fxml;
-    exports com.example.atividadelp1;
-}
